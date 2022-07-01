5148cfd70043eec229a58b80a3cc47c743203d13
GiliEllen <gilimena95@gmail.com>
2022-07-01 09:39:00 +0300

    working on editprofile.ts
    sending the info

--- Main/public/dist/editProfile.js
+++ Main/public/dist/editProfile.js
@@ -37,10 +37,36 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 console.log("connected");
 function handleEditUser(event) {
     return __awaiter(this, void 0, void 0, function () {
-        var email;
-        return __generator(this, function (_a) {
-            email = event;
-            return [2 /*return*/];
+        var _a, email, username, job, address, profilePic, data, error_1;
+        return __generator(this, function (_b) {
+            switch (_b.label) {
+                case 0:
+                    event.preventDefault();
+                    _b.label = 1;
+                case 1:
+                    _b.trys.push([1, 4, , 5]);
+                    _a = event.target.elements, email = _a.email, username = _a.username, job = _a.job, address = _a.address, profilePic = _a.profilePic;
+                    email = email.value;
+                    username = username.value;
+                    job = job.value;
+                    address = address.value;
+                    profilePic = profilePic.value;
+                    return [4 /*yield*/, axios.post("/users/edit-user", {
+                            email: email,
+                            username: username,
+                            job: job,
+                            address: address,
+                            profilePic: profilePic
+                        })];
+                case 2: return [4 /*yield*/, _b.sent()];
+                case 3:
+                    data = (_b.sent()).data;
+                    return [3 /*break*/, 5];
+                case 4:
+                    error_1 = _b.sent();
+                    return [3 /*break*/, 5];
+                case 5: return [2 /*return*/];
+            }
         });
     });
 }
--- Main/public/editProfile.ts
+++ Main/public/editProfile.ts
@@ -1,5 +1,23 @@
 console.log(`connected`)
 
 async function handleEditUser(event) {
-    const email = event
+    event.preventDefault()
+    try {
+        let { email, username, job, address,profilePic} = event.target.elements;
+    email = email.value;
+    username = username.value;
+    job = job.value;
+    address = address.value;
+    profilePic = profilePic.value;
+
+    const { data } = await await axios.post("/users/edit-user", {
+        email,
+        username,
+        job,
+        address,
+        profilePic,
+      });
+    } catch (error) {
+        
+    }
 }
